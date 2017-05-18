986cc700e286e795c17d25644259e8604d233fd0
mitin.sharma1@marist.edu <mitin.sharma1@marist.edu>
2017-05-18 15:35:01 -0400

    routes, about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-   <user></user>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+  </ul>
+   <router-outlet></router-outlet>
   `,
 })
 export class AppComponent  {
---
+++ src/app/components/about.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  template: `
+   <h2>About this app</h2>
+  `,
+})
+export class AboutComponent  {
+}
